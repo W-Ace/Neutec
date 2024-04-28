8e5a2c7a951a3782585bbef32f663d792d9c4295
Fronten fighter <f0936360019@gmail.com>
2024-04-28 19:56:22 +0800

    chore(vite-config): modify base url on production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/Neutec/' : '/',
   plugins: [
     vue(),
     createSvgIconsPlugin({
